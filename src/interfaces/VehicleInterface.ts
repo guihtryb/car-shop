922dd8df8da356d94f247c3b236a5c7d712a13fe
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-06 16:45:16 -0300

    feat: adding vehicle interface - concludes requisite 2

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose';
+import { z } from 'zod';
+
+const VehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().positive().int(),
+});
+
+export type Vehicle = z.infer<typeof VehicleSchema>;
+
+export const vehicleMongooseSchema = new Schema<Vehicle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+});
