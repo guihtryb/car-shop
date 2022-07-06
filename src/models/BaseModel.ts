589dd2704f9f205f08c84a4fd2ee6b7952286f44
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-06 19:03:36 -0300

    feat: add abstract model

---
+++ src/models/BaseModel.ts
@@ -0,0 +1,27 @@
+import { Model as MongoModel } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+export default abstract class BaseModel<T> implements Model<T> {
+  constructor(
+    protected model: MongoModel<T>,
+  ) { }
+
+  public create = async (payload: T): Promise<T> => this.model.create(payload);
+
+  public read = async (): Promise<T[]> => this.model.find();
+
+  public readOne = async (_id: string): Promise<T | null> => this
+    .model.findOne({ _id });
+
+  public update = async (_id: string, payload: T): Promise<T | null> => {
+    await this.model.updateOne({ _id }, { payload });
+
+    return this.model.findById(_id);
+  };
+
+  public delete = async (_id: string): Promise<T | null> => {
+    await this.model.deleteOne({ _id });
+
+    return this.model.findById(_id);
+  };
+}
