b158ff0f604f9f61961486f4ea5f9a069859ede8
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-06 19:02:46 -0300

    update: change id param to use short-hand on model methods

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model<T> {
   create(payload: T): Promise<T>;
   read(): Promise<T[]>;
-  readOne(id: string): Promise<T | null>;
-  update(id: string, payload: T): Promise<T | null>;
-  delete(id: string): Promise<T | null>;
+  readOne(_id: string): Promise<T | null>;
+  update(_id: string, payload: T): Promise<T | null>;
+  delete(_id: string): Promise<T | null>;
 }
